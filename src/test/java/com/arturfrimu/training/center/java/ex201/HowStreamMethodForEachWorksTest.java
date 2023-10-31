be62dd770a0eab55afb35fab964b2e44f14d510d
Artur Frimu <artur.frimu2@gmail.com>
2023-10-31 16:41:37 +0200

    Fix tests

--- src/test/java/com/arturfrimu/training/center/java/ex201/HowStreamMethodForEachWorksTest.java
+++ src/test/java/com/arturfrimu/training/center/java/ex201/HowStreamMethodForEachWorksTest.java
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.Test;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+/**
+ * @see <a href="https://www.javatpoint.com/java-8-method-reference">Method Reference</a>
+ * @see <a href="https://www.geeksforgeeks.org/stream-foreach-method-java-examples/">Method forEach</a>
+ * @see <a href="https://www.geeksforgeeks.org/java-8-consumer-interface-in-java-with-examples/">Java Consumer</a>
+ */
 @Slf4j
 class HowStreamMethodForEachWorksTest {
 
-    /**
-     * @see <a href="https://www.geeksforgeeks.org/stream-foreach-method-java-examples/">Method forEach</a>
-     * @see <a href="https://www.geeksforgeeks.org/java-8-consumer-interface-in-java-with-examples/">Java Consumer</a>
-     */
     @Test
     void testForEachWithIntegers() {
         Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
@@ -22,14 +23,10 @@ class HowStreamMethodForEachWorksTest {
         integerStream.forEach(printlnInConsole);
     }
 
-    /**
-     * @see <a href="https://www.javatpoint.com/java-8-method-reference">Method Reference</a>
-     */
     @Test
     void testForEachWithStrings() {
         Stream<String> stringStream = Stream.of("one", "two", "three");
 
-        // method reference  Object::method
         Consumer<String> printlnInConsole = System.out::println;
 
         stringStream.forEach(printlnInConsole);
--- src/test/java/com/arturfrimu/training/center/java/ex202/HowStreamMethodMapWorksTest.java
+++ src/test/java/com/arturfrimu/training/center/java/ex202/HowStreamMethodMapWorksTest.java
@@ -3,9 +3,12 @@ package com.arturfrimu.training.center.java.ex202;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @see <a href=https://www.geeksforgeeks.org/functional-interfaces-java/?ref=lbp">Functional Interfaces in Java</a>
@@ -18,52 +21,49 @@ class HowStreamMethodMapWorksTest {
 
     @Test
     void testMapWithIntegersV1() {
-        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
 
-        Function<Integer, Integer> multiplyByTwo = number -> number * 2;
-        Consumer<Integer> printInConsole = System.out::println;
+        List<Integer> newNumbers = numbers
+                .stream()
+                .map(number -> number * 2)
+                .toList();
 
-        integerStream.map(multiplyByTwo).forEach(printInConsole);
+        assertThat(newNumbers).isEqualTo(List.of(2, 4, 6, 8, 10));
     }
 
     @Test
     void testMapWithIntegersV2() {
-        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
 
-        Function<Integer, String> multiplyByTwo = number -> "[ " + number + " ]";
-        Consumer<String> printInConsole = System.out::println;
+        List<String> newNumbers = numbers
+                .stream()
+                .map(number -> "[ " + number + " ]")
+                .toList();
 
-        integerStream.map(multiplyByTwo).forEach(printInConsole);
+        assertThat(newNumbers).isEqualTo(List.of("[ 1 ]", "[ 2 ]", "[ 3 ]", "[ 4 ]", "[ 5 ]"));
     }
 
     @Test
     void testMapWithStringsV1() {
-        Stream<String> stringStream = Stream.of("one", "two", "three");
+        List<String> strings = List.of("one", "two", "three");
 
-        Function<String, String> addExclamationsAtTheEnd = string -> string + "!!!";
-        Consumer<String> printInConsole = System.out::println;
+        List<String> newStrings = strings
+                .stream()
+                .map(String::toUpperCase)
+                .toList();
 
-        stringStream.map(addExclamationsAtTheEnd).forEach(printInConsole);
+        assertThat(newStrings).isEqualTo(List.of("ONE", "TWO", "THREE"));
     }
 
     @Test
     void testMapWithStringsV2() {
-        Stream<String> stringStream = Stream.of("1", "2", "3", "4", "5");
+        List<String> strings = List.of("1", "2", "3", "4", "5");
 
-        Function<String, Integer> addExclamationsAtTheEnd = string -> Integer.parseInt(string);
-        Consumer<Integer> printInConsole = System.out::println;
+        List<Integer> newStrings = strings
+                .stream()
+                .map(Integer::parseInt)
+                .toList();
 
-        stringStream.map(addExclamationsAtTheEnd).forEach(printInConsole);
-    }
-
-    @Test
-    void testMapWithStringsV3() {
-        Stream<String> stringStream = Stream.of("1", "2", "3", "4", "5");
-
-        // method reference  Object::method
-        Function<String, Integer> addExclamationsAtTheEnd = Integer::parseInt;
-        Consumer<Integer> printInConsole = System.out::println;
-
-        stringStream.map(addExclamationsAtTheEnd).forEach(printInConsole);
+        assertThat(newStrings).isEqualTo(List.of(1, 2, 3, 4, 5));
     }
 }
