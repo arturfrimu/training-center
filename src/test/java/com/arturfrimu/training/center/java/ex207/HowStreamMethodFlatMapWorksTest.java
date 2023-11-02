3ff92a9d1d7e956565edf8d8ea068845a79002f6
Artur Frimu <artur.frimu2@gmail.com>
2023-11-02 11:38:30 +0200

    List<Integer> sortedList = numbers.stream().sorted().collect(Collectors.toList());

--- src/test/java/com/arturfrimu/training/center/java/ex207/HowStreamMethodFlatMapWorksTest.java
+++ src/test/java/com/arturfrimu/training/center/java/ex207/HowStreamMethodFlatMapWorksTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
 class HowStreamMethodFlatMapWorksTest {
---
+++ src/test/java/com/arturfrimu/training/center/java/ex208/HowStreamMethodSortedWorksTest.java
@@ -0,0 +1,22 @@
+package com.arturfrimu.training.center.java.ex208;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class HowStreamMethodSortedWorksTest {
+
+    @Test
+    void testSorted() {
+        List<Integer> numbers = List.of(3, 1, 2);
+
+        List<Integer> sortedList = numbers.stream().sorted().collect(Collectors.toList());
+
+        assertThat(sortedList).containsExactly(1, 2, 3);
+    }
+}
