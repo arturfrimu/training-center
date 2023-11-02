0f28d921119167a4d6bff2b10bffd4d8580d515a
Artur Frimu <artur.frimu2@gmail.com>
2023-11-02 11:42:16 +0200

    List<Integer> limitedList = numbers.stream().limit(3).toList();

---
+++ src/test/java/com/arturfrimu/training/center/java/ex210/HowStreamMethodLimitWorksTest.java
@@ -0,0 +1,21 @@
+package com.arturfrimu.training.center.java.ex210;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class HowStreamMethodLimitWorksTest {
+
+    @Test
+    void testLimit() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        List<Integer> limitedList = numbers.stream().limit(3).toList();
+
+        assertThat(limitedList).containsExactly(1, 2, 3);
+    }
+}
