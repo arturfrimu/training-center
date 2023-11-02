8a2590add4ecf929d4ece917f4e411e503f01ac3
Artur Frimu <artur.frimu2@gmail.com>
2023-11-02 12:27:00 +0200

    Stream<Integer> sortedUnorderedStream = Stream.of(4, 3, 2, 1).sorted().unordered();

---
+++ src/test/java/com/arturfrimu/training/center/java/ex221/HowStreamMethodUnorderedWorksTest.java
@@ -0,0 +1,19 @@
+package com.arturfrimu.training.center.java.ex221;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class HowStreamMethodUnorderedWorksTest {
+
+    @Test
+    void testUnordered() {
+        Stream<Integer> sortedUnorderedStream = Stream.of(4, 3, 2, 1).sorted().unordered();
+        assertThat(sortedUnorderedStream.collect(Collectors.toList())).containsExactlyInAnyOrder(2, 1, 4, 3);
+    }
+}
