c8985c4051460ae7063b0a66a9e553846b36b387
Artur Frimu <artur.frimu2@gmail.com>
2023-11-02 11:34:56 +0200

    List<Integer> flattenedList = nestedLists.stream().flatMap(List::stream).collect(Collectors.toList());

---
+++ src/test/java/com/arturfrimu/training/center/java/ex207/HowStreamMethodFlatMapWorksTest.java
@@ -0,0 +1,23 @@
+package com.arturfrimu.training.center.java.ex207;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Slf4j
+class HowStreamMethodFlatMapWorksTest {
+
+    @Test
+    void testFlatMap() {
+        List<List<Integer>> nestedLists = List.of(List.of(1, 2), List.of(3, 4));
+
+        List<Integer> flattenedList = nestedLists.stream().flatMap(List::stream).collect(Collectors.toList());
+
+        assertThat(flattenedList).containsExactly(1, 2, 3, 4);
+    }
+}
