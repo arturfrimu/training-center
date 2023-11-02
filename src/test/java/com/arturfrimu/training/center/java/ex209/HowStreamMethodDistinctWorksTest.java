c7dc9933639a55e75716f97cce8d8d3eeed6c02f
Artur Frimu <artur.frimu2@gmail.com>
2023-11-02 11:41:14 +0200

    List<Integer> distinctList = numbers.stream().distinct().toList();

---
+++ src/test/java/com/arturfrimu/training/center/java/ex209/HowStreamMethodDistinctWorksTest.java
@@ -0,0 +1,30 @@
+package com.arturfrimu.training.center.java.ex209;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class HowStreamMethodDistinctWorksTest {
+
+    @Test
+    void testDistinct() {
+        List<Integer> numbers = List.of(1, 2, 2, 3, 3, 3);
+
+        List<Integer> distinctList = numbers.stream().distinct().toList();
+
+        assertThat(distinctList).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    void testDistinctStrings() {
+        List<String> numbers = List.of("1", "2", "2", "3", "3", "3");
+
+        List<String> distinctList = numbers.stream().distinct().toList();
+
+        assertThat(distinctList).containsExactly("1", "2", "3");
+    }
+}
