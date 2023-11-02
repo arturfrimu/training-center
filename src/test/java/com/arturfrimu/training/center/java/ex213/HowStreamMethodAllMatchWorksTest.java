00ee0481fb32529ccaac0fb4d808824fdebf347f
Artur Frimu <artur.frimu2@gmail.com>
2023-11-02 11:48:14 +0200

    boolean allGreaterThanZero = numbers.stream().allMatch(n -> n > 0);

---
+++ src/test/java/com/arturfrimu/training/center/java/ex213/HowStreamMethodAllMatchWorksTest.java
@@ -0,0 +1,21 @@
+package com.arturfrimu.training.center.java.ex213;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class HowStreamMethodAllMatchWorksTest {
+
+    @Test
+    void testAllMatch() {
+        List<Integer> numbers = List.of(1, 2, 3);
+
+        boolean allGreaterThanZero = numbers.stream().allMatch(n -> n > 0);
+
+        assertThat(allGreaterThanZero).isTrue();
+    }
+}
