854d82380e53c9e59bcc6feb8ddbab7263cc3db7
Artur Frimu <artur.frimu2@gmail.com>
2023-11-02 12:13:02 +0200

    IntStream rangeClosedStream = IntStream.rangeClosed(1, 3); // 1 to 3

--- src/test/java/com/arturfrimu/training/center/java/ex221/HowStreamMethodRangeWorksTest.java
+++ src/test/java/com/arturfrimu/training/center/java/ex221/HowStreamMethodRangeWorksTest.java
@@ -3,6 +3,7 @@ package com.arturfrimu.training.center.java.ex221;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,5 +16,12 @@ class HowStreamMethodRangeWorksTest {
         IntStream rangeStream = IntStream.range(1, 4); // 1 to 3
 
         assertThat(rangeStream.toArray()).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    void testRangeClosed() {
+        IntStream rangeClosedStream = IntStream.rangeClosed(1, 3); // 1 to 3
+
+        assertThat(rangeClosedStream.toArray()).containsExactly(1, 2, 3);
     }
 }
