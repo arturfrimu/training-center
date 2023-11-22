cd4d2a7db26bd7e057ac5d97a22b137faea0ecd3
Artur Frimu <artur.frimu2@gmail.com>
2023-11-22 23:22:28 +0200

    About Integer

---
+++ src/main/java/com/arturfrimu/training/center/junior/java/section2/Lesson2.java
@@ -0,0 +1,14 @@
+package com.arturfrimu.training.center.junior.java.section2;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        int minim = Integer.MIN_VALUE;
+        int maxim = Integer.MAX_VALUE;
+
+        System.out.println("minim = " + minim);
+        System.out.println("maxim = " + maxim);
+
+        System.out.println(Integer.MAX_VALUE + 1 == Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE - 1 == Integer.MAX_VALUE);
+    }
+}
