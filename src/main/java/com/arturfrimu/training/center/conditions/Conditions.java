ecd9d90952bc20767e5a56ac8053d6b216ee8e71
Artur Frimu <artur.frimu2@gmail.com>
2024-01-11 18:31:16 +0200

    Conditions in java

---
+++ src/main/java/com/arturfrimu/training/center/conditions/Conditions.java
@@ -0,0 +1,13 @@
+package com.arturfrimu.training.center.conditions;
+
+public class Conditions {
+    public static void main(String[] args) {
+        int temperaturaDeAfara = -1;
+
+        if (temperaturaDeAfara < 0) {
+            System.out.println("Imbracate gros");
+        } else {
+            System.out.println("Imbracate subtire");
+        }
+    }
+}
