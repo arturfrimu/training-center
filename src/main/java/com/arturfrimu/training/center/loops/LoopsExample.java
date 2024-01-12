92004dd8f5a46d33c7a993827b879963f2b4826b
Artur Frimu <artur.frimu2@gmail.com>
2024-01-12 09:16:51 +0200

    Loops in java

---
+++ src/main/java/com/arturfrimu/training/center/loops/LoopsExample.java
@@ -0,0 +1,9 @@
+package com.arturfrimu.training.center.loops;
+
+public class LoopsExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
