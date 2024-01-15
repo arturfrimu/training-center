a650f60156cec174178330f8127b2dc2bb33a529
Artur Frimu <artur.frimu2@gmail.com>
2024-01-15 09:47:04 +0200

    Switch case

--- src/main/java/com/arturfrimu/training/center/loops/LoopsExample.java
+++ src/main/java/com/arturfrimu/training/center/loops/LoopsExample.java
@@ -14,5 +14,32 @@ public class LoopsExample {
         do {
             System.out.println(nr--);
         } while (nr > 0);
+
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("LUNI");
+                break;
+            case 2:
+                System.out.println("MARTI");
+                break;
+            case 3:
+                System.out.println("MIERCURI");
+                break;
+            case 4:
+                System.out.println("JOI");
+                break;
+            case 5:
+                System.out.println("VINERI");
+                break;
+            case 6:
+                System.out.println("SAMBATA");
+                break;
+            case 7:
+                System.out.println("DUMINICA");
+                break;
+            default:
+                System.out.println("NU EXISTA ASA ZI A SAPTAMANII");
+        }
     }
 }
